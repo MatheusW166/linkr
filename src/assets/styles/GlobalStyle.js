46728bfc3b670dd806bb597c184ee0e21c4efe97
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-05-30 19:49:01 -0300

    style: input reset and button hover effects

--- src/assets/styles/GlobalStyle.js
+++ src/assets/styles/GlobalStyle.js
@@ -4,6 +4,17 @@ const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
   }
+  body {
+    background: #333333;
+    color: #fff;
+    font-family: 'Lato', sans-serif;
+  }
+  button {
+    cursor: pointer;
+    :hover {
+      opacity: 0.8;
+    }
+  }
 `;
 
 export default GlobalStyle;
