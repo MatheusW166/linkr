68f709e209b36c88422c509c99149774a0dd4a2e
SolForte <mateusbrandao1234@gmail.com>
2023-06-01 15:05:29 -0300

    feat: username's posts

--- src/pages/UserPage/index.jsx
+++ src/pages/UserPage/index.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { TitleH2Styled } from '../../styled';
 import { MainStyled, PageContainerStyled } from './styled';
 import Header from '../../components/Header';
 import PostsList from '../../components/PostsList';
@@ -12,8 +13,17 @@ export default function UserPage() {
   const [loadingPosts, setLoadingPosts] = useState(true);
   const [posts, setPosts] = useState(null);
   const [errorPosts, setErrorPosts] = useState(null);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/search/user/${id}`)
+      .then((res) => {
+        setUser(res.data);
+      })
+      .catch(() => {
+      });
+
     axios
       .get(`${process.env.REACT_APP_API_URL}/search/posts/${id}`)
       .then((res) => {
@@ -31,6 +41,10 @@ export default function UserPage() {
       <Header />
       <PageContainerStyled>
         <MainStyled>
+          <TitleH2Styled>
+            {user ? user?.name : 'User'}
+            &apos;s posts
+          </TitleH2Styled>
           <PostsList posts={posts} error={errorPosts} loading={loadingPosts} />
         </MainStyled>
       </PageContainerStyled>
