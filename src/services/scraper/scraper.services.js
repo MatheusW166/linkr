60a080365cdb5ba7716de4d62192fd82c44d0415
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-06-02 12:41:46 -0300

    ref: new api for extracting url metadata

--- src/services/scraper/scraper.services.js
+++ src/services/scraper/scraper.services.js
@@ -1,20 +1,10 @@
 import scraperClient from './scraper.client';
 
-const key = process.env.REACT_APP_SCRAPER_PROXY_KEY;
-
 export default async function scrapUrl({ url }) {
-  const response = await scraperClient.get(`?api_key=${key}&url=${url}`);
-  const html = response.data;
+  const response = await scraperClient.get(`?url=${url}`);
+  const metadata = response.data;
 
-  const description = html
-    .match(/<meta\s+name=["']description["']\s+content=["'](.*?)["']\s*\/?>/i)
-    ?.at(1);
+  const { description, title, images } = metadata;
 
-  const title = html.match(/<title[^>]*>(.*?)<\/title>/i)?.at(1);
-
-  const icon = html
-    .match(/<link[^>]*rel=["']icon["'][^>]*href=["'](.*?)["'][^>]*>/i)
-    ?.at(1);
-
-  return { title, description, icon };
+  return { title, description, icon: images[0] };
 }
