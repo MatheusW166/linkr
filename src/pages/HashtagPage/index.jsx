74dd00080ff5309f19b3fddb3236d31577b0e8da
Victor Lahr <lahr.victor@gmail.com>
2023-06-04 15:49:43 -0300

    feat: implement hashtag page

---
+++ src/pages/HashtagPage/index.jsx
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Header from '../../components/Header';
+import PostsList from '../../components/PostsList';
+import { TitleH2Styled } from '../../styled';
+import { MainStyled, PageContainerStyled } from './styled';
+
+export default function HashtagPage() {
+  const { hashtag } = useParams();
+
+  const [loadingPosts, setLoadingPosts] = useState(true);
+  const [posts, setPosts] = useState(null);
+  const [errorPosts, setErrorPosts] = useState(null);
+  const [user, setUser] = useState(null);
+
+  const { REACT_APP_API_URL } = process.env;
+  const token = localStorage.getItem('token');
+
+  function retrievePosts(hashtagName) {
+    axios.get(`${REACT_APP_API_URL}/hashtag/${hashtagName}`, { headers: { Authorization: `Bearer ${token}` } })
+      .then((response) => {
+        setPosts(response.data);
+        setLoadingPosts(false);
+      })
+
+      .catch((error) => {
+        // eslint-disable-next-line no-alert
+        alert(error.response.data);
+        setErrorPosts(error);
+        setLoadingPosts(false);
+      });
+  }
+
+  useEffect(() => {
+    retrievePosts(hashtag);
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <PageContainerStyled>
+        <MainStyled>
+          <TitleH2Styled>
+            #
+            {hashtag}
+          </TitleH2Styled>
+          <PostsList
+            error={errorPosts}
+            loading={loadingPosts}
+            posts={posts}
+          />
+        </MainStyled>
+      </PageContainerStyled>
+    </>
+  );
+}
---
+++ src/pages/HashtagPage/styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const PageContainerStyled = styled.div`
+  margin: 0 auto;
+  padding-top: calc(72px + 64px);
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const MainStyled = styled.main`
+  display: flex;
+  flex-direction: column;
+  width: min(616px, 100%);
+  gap: 40px;
+  margin: 0 auto;
+
+  h2 {
+    @media screen and (max-width: 616px) {
+      margin-left: 24px;
+    }
+  }
+`;
