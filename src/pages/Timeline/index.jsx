b4025e9dfb2aff1653f37766545e6e2b7785c0e6
santiago <santiagorsilva99@gmail.com>
2023-06-08 19:50:13 -0300

    fix: timeline page

--- src/pages/Timeline/index.jsx
+++ src/pages/Timeline/index.jsx
@@ -1,26 +1,34 @@
-/* eslint-disable no-alert */
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import useInterval from 'use-interval';
+import { BiRefresh } from 'react-icons/bi';
 import {
   MainStyled,
   PageContainerStyled,
   SectionStyled,
   PostsStyled,
+  UpdateButton,
 } from './styled';
-import { TitleH2Styled } from '../../styled';
+import ScrollIndicator from '../../components/ScrollIndicator';
+import { LoaderStyled, TitleH2Styled } from '../../styled';
 import CreatePost from '../../components/CreatePost';
 import Header from '../../components/Header';
-import { useMutation, useRequest } from '../../hooks/request.hooks';
-import { getUserFollowers, publishPost, searchPosts } from '../../services/api/timeline.services';
+import { useMutation } from '../../hooks/request.hooks';
+import usePostsPagination from '../../hooks/posts.hooks';
+import { publishPost } from '../../services/api/timeline.services';
 import PostsList from '../../components/PostsList';
 import TrendingStyled from '../../components/Trending';
+import client from '../../services/api/api.client';
 
 export default function Timeline() {
+  const [updatedPosts, setUpdatedPosts] = useState([]);
+
   const {
     data: posts,
     loading: loadingPosts,
     error: errorPosts,
     refresh: refreshPosts,
-  } = useRequest(searchPosts);
+    nextPage,
+  } = usePostsPagination(10);
 
   const { mutate: publish, loading: loadingPublish } = useMutation(publishPost);
 
@@ -37,10 +45,25 @@ export default function Timeline() {
         event.target.reset();
       },
       onError: () => {
-        alert('Não foi possível publicar');
+        alert('There was an error publishing your link');
       },
     });
   };
+
+  const loadingNewPosts = loadingPosts && posts?.length;
+
+  const handleUpdatePosts = (event) => {
+    event.preventDefault();
+    refreshPosts();
+  };
+
+  useInterval(() => {
+    client.get('/posts').then((res) => {
+      if (res.data.length !== posts.length) {
+        setUpdatedPosts(res.data);
+      }
+    });
+  }, 15000);
 
   const [followedUsers, setFollowedUsers] = useState([]);
 
@@ -65,7 +88,25 @@ export default function Timeline() {
           <TitleH2Styled>timeline</TitleH2Styled>
           <SectionStyled>
             <PostsStyled>
-              <CreatePost loading={loadingPublish} onSubmit={handlePostSubmit} />
+              <UpdateButton
+                update={posts && updatedPosts.length - posts.length > 0 && !NaN}
+                onClick={handleUpdatePosts}
+              >
+                {posts && updatedPosts.length - posts.length > 0 && !NaN ? (
+                  <p>
+                    {updatedPosts.length - posts.length}
+                    {' '}
+                    new posts, load more!
+                    <BiRefresh />
+                  </p>
+                ) : (
+                  ''
+                )}
+              </UpdateButton>
+              <CreatePost
+                loading={loadingPublish}
+                onSubmit={handlePostSubmit}
+              />
               <PostsList
                 posts={posts}
                 error={errorPosts}
@@ -74,8 +115,10 @@ export default function Timeline() {
                 followedUsers={followedUsers}
                 page="timeline"
               />
+              {posts?.length && <ScrollIndicator onIntersection={nextPage} />}
+              {loadingNewPosts && <LoaderStyled />}
             </PostsStyled>
-            <TrendingStyled />
+            <TrendingStyled posts={posts} />
           </SectionStyled>
         </MainStyled>
       </PageContainerStyled>
